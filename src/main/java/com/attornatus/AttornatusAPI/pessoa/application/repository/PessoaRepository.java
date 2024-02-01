5b11e9223845495dddc1506c144e2f8d4bb37d6c
Rauan Santos <rauanmor@gmail.com>
2024-02-01 20:17:32 -0300

    implementa fluxo repository - testado

--- src/main/java/com/attornatus/AttornatusAPI/pessoa/application/repository/PessoaRepository.java
+++ src/main/java/com/attornatus/AttornatusAPI/pessoa/application/repository/PessoaRepository.java
@@ -2,6 +2,10 @@ package com.attornatus.AttornatusAPI.pessoa.application.repository;
 
 import com.attornatus.AttornatusAPI.pessoa.dominio.Pessoa;
 
+import java.util.UUID;
+
 public interface PessoaRepository {
     Pessoa salva(Pessoa pessoa);
+
+    Pessoa buscaPessoaAtravesId(UUID idPessoa);
 }
