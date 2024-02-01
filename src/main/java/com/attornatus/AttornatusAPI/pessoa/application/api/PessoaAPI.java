6060e01ae9b151a05e280e3fbc1fc7ce62392d89
Rauan Santos <rauanmor@gmail.com>
2024-02-01 20:15:12 -0300

    implementa fluxo controller - service

--- src/main/java/com/attornatus/AttornatusAPI/pessoa/application/api/PessoaAPI.java
+++ src/main/java/com/attornatus/AttornatusAPI/pessoa/application/api/PessoaAPI.java
@@ -4,10 +4,16 @@ import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.UUID;
+
 @RestController
 @RequestMapping("/v1/pessoa")
 public interface PessoaAPI {
     @PostMapping
     @ResponseStatus(code = HttpStatus.CREATED)
     PessoaResponse postPessoa (@Valid @RequestBody PessoaResquest pessoaResquest);
+
+    @GetMapping(value = "/{idPessoa}")
+    @ResponseStatus(code = HttpStatus.OK)
+    PessoaResponse getPessoaAtravesId(@PathVariable UUID idPessoa);
 }
